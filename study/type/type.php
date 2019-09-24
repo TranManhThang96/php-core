8a93c9668559f1c0510178adc79a4a0810059a76
TranManhThang <tranmanhthang96@gmail.com>
2019-09-24 23:41:15 +0700

    'type'

---
+++ study/type/type.php
@@ -0,0 +1,13 @@
+<?php
+$a_bool = TRUE;   // boolean
+$a_str  = "foo";  // string
+$a_str2 = 'foo';  // string
+$an_int = 12;     // integer
+$b = 1.2;         // double
+$c = [];          //array
+
+$x = 8 - 6.4;
+$y = 1.6;
+if (round($x, 5) === round($y, 5)) {
+    var_dump('thangbeo');
+}
