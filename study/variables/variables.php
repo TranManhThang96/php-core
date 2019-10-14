bb98dec926cef99b9bbf7b9192b599ede66b94a6
TranManhThang <tranmanhthang96@gmail.com>
2019-10-14 21:14:07 +0700

    constant

--- study/variables/variables.php
+++ study/variables/variables.php
@@ -1,9 +1,33 @@
 <?php
-function test()
+$thang = 'huyen';
+$$thang = 'thangbeo';
+echo $huyen;
+
+define('MIN_VALUE', '0.0');   // RIGHT - Works OUTSIDE of a class definition.
+define('MAX_VALUE', '1.0');   // RIGHT - Works OUTSIDE of a class definition.
+
+//const MIN_VALUE = 0.0;         RIGHT - Works both INSIDE and OUTSIDE of a class definition.
+//const MAX_VALUE = 1.0;         RIGHT - Works both INSIDE and OUTSIDE of a class definition.
+
+class Constants
 {
-    $a = 0;
-    echo $a;
-    $a++;
+    //define('MIN_VALUE', '0.0');  WRONG - Works OUTSIDE of a class definition.
+    //define('MAX_VALUE', '1.0');  WRONG - Works OUTSIDE of a class definition.
+    define('HAHA', 'ko dùng được');
+    const MIN_VALUE = 10.0;      // RIGHT - Works INSIDE of a class definition.
+    const MAX_VALUE = 1.0;      // RIGHT - Works INSIDE of a class definition.
+
+    public static function getMinValue()
+    {
+        echo self::MIN_VALUE;
+        return self::MIN_VALUE;
+    }
+
+    public static function getMaxValue()
+    {
+        return self::MAX_VALUE;
+    }
 }
 
-test();
+$con = new Constants();
+$con->getMinValue();
