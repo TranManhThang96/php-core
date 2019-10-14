a864c5ca44cef81ae99115d6390322490bd33dc1
TranManhThang <tranmanhthang96@gmail.com>
2019-10-14 22:54:49 +0700

    'class'

--- study/variables/variables.php
+++ study/variables/variables.php
@@ -13,7 +13,6 @@ class Constants
 {
     //define('MIN_VALUE', '0.0');  WRONG - Works OUTSIDE of a class definition.
     //define('MAX_VALUE', '1.0');  WRONG - Works OUTSIDE of a class definition.
-    define('HAHA', 'ko dùng được');
     const MIN_VALUE = 10.0;      // RIGHT - Works INSIDE of a class definition.
     const MAX_VALUE = 1.0;      // RIGHT - Works INSIDE of a class definition.
 
