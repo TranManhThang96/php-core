96fd57b5532032e9c1d74c4735cf3db54aeaa4cf
TranManhThang <tranmanhthang96@gmail.com>
2021-03-18 23:16:57 +0700

    'update'

---
+++ study/oop/inheritance/ex1.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Unless autoloading is used, the classes must be defined before they are used
+ * Trừ khi autoloading được sử dụng, class phải được định nghĩa trước khi sử dụng.
+ * If a class extends another, then the parent class must be declared before the child class structure
+ * Nếu một lớp được mở rộng từ lớp khác, thì lớp cha phải được định nghĩa trước lớp con.
+ * PHP chỉ hỗ trợ đơn kế thừa (nghĩa là 1 lớp chỉ được kế thừa duy nhất 1 lớp khác)
+ */
